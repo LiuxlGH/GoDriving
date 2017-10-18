5e2f19cd4f4c805750f23ebe6b45ceccf01103f9
Liuxl <569072733@qq.com>
2017-10-18 12:16:44 +0800

    baidu tts

---
+++ app/src/main/java/com/liuxl/godriving/service/HWReceiver.java
@@ -0,0 +1,17 @@
+package com.liuxl.godriving.service;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Liuxl on 2017/9/21.
+ */
+
+public class HWReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(getClass().getName(),"intent : ");
+    }
+}
